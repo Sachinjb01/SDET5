ed3006dc4a89d8cc05d347f2a4ad796a31d1194f
Sachinjb01 <sachinjb96@gmail.com>
2022-01-31 22:14:51 +0530

    mine first commit

---
+++ src/test/java/gitproject1/samplegit.java
@@ -0,0 +1,12 @@
+package gitproject1;
+
+public class samplegit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is mine first commit");
+
+	}
+
+}
